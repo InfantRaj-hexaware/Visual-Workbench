9a5d446e261f0264a086f60a14780f7fe9a0b5ed
InfantRaj-hexaware <72547608+InfantRaj-hexaware@users.noreply.github.com>
2023-03-06 11:03:58 +0530

    Add files via upload

---
+++ Backend/Master Backend Files/Coding Files/constants.py
@@ -0,0 +1,49 @@
+import pyaudio
+
+
+# CONSTANTS --> AUDIO SETTINGS
+FRAMES_PER_BUFFER = 5000
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+FRAME_RATE = 20000
+SECONDS = 30
+
+# CONSTANT --> OUTPUT FILE NAME
+RECORDING_OUTPUT_FILE_NAME = "male.wav"
+TRANSCRIPT_OUTPUT_FILE_NAME = "save_transcript.txt"
+
+
+# CONSTANT --> ASSEMBLY.AI API KEY
+# SUBSCRIPTION_KEY = "a0ba86dc6a004985b38c9f3039908670"
+SUBSCRIPTION_KEY = "b0050fa2fd2a4ed59c01f3bbd915b376"
+UPLOAD_ENDPOINT = "https://api.assemblyai.com/v2/upload"
+TRANSCRIPT_ENDPOINT = "https://api.assemblyai.com/v2/transcript"
+
+# CONSTANT --> AZURE SPEECH API KEY
+AZ_SUBSCRIPTION_KEY = "b0050fa2fd2a4ed59c01f3bbd915b376"
+AZ_LOCATION_ID = "centralindia" 
+
+# CONSTANT --> DETECT HEADERS AND CONTENT
+HEADERS_AUTH_ONLY = {'authorization': SUBSCRIPTION_KEY}
+HEADERS = {
+    "authorization": SUBSCRIPTION_KEY,
+    "content-type": "application/json"
+}
+
+CHUNK_SIZE = 5242880  # 5MB
+
+
+#for local
+# CONSTANT --> RECORDING FILE PATH
+RECORDING_FILE_PATH = "D:\\CTO\\SpeechToText\\DebiTeam\\AzureModel\\recording_output\\"
+
+# CONSTANT --> TRANSCRIPTION FILE PATH
+TRANSCRIPTION_FILE_PATH = "D:\\CTO\\SpeechToText\\DebiTeam\\AzureModel\\transcription_output\\"
+
+
+# for server
+# RECORDING_FILE_PATH = "/home/vaas_bi/backend/"
+# TRANSCRIPTION_FILE_PATH = "/home/vaas_bi/backend/transcription_output/"
+
+
+
